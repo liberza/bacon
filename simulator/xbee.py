b6132fe430134b597bbc22f2b7dc185859763e31
Nick Levesque <nick@levesque.io>
2016-10-15 11:40:26 -0400

    working on escape function and init in xbee.py

--- simulator/xbee.py
+++ simulator/xbee.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python3
 import serial
+import queue
 
 # class for managing XBee API mode 
 class XBee():
-    def __init__(self, baud, network_id):
+    FRAME_DELIM = b'\x7E'
+    ESCAPE = b'\x7D'
+    XON = b'\x11'
+    XOFF = b'\x13'
+
+    SPECIAL_BYTES = (FRAME_DELIM, ESCAPE, XON, XOFF)
+    
+    rx_bytes = bytearray()
+    rx_queue = queue.Queue()
+
+    def __init__(self, devfile, baud):
+        self.serial = serial.Serial(port=devfile, 
+                                    baudrate=baud,
+                                    parity=serial.PARITY_NONE,
+                                    stopbits=serial.STOPBITS_ONE,
+                                    bytesize=serial.EIGHTBITS
+                                    )
+
+    def tx(data, dest=0x000000000000FFFF, opts=0x00):
         pass
+
+    def escape(data):
+        escaped = b''
+        for byte in data:
+            if byte in SPECIAL_BYTES:
+                escaped += ESCAPE
+                escaped += byte ^ 0x20
+            else:
+                escaped += byte
