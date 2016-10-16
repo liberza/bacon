a62729e16b48633821668288ac1481983e238051
Nick Levesque <nick@levesque.io>
2016-10-16 16:39:35 -0400

    reorganize constants, add new FRAME_TYPES dict of constants.

--- simulator/xbee.py
+++ simulator/xbee.py
@@ -5,18 +5,32 @@ import time
 
 # class for managing XBee API mode 
 class XBee():
-    FRAME_DELIM = 0x7E
-    ESCAPE = 0x7D
-    XON = 0x11
-    XOFF = 0x13
+    SPECIAL_BYTES = {
+        'FRAME_DELIM':  0x7E,
+        'ESCAPE':       0x7D,
+        'XON':          0x11,
+        'XOFF':         0x13,
+    }
 
-    FRAME_TX = 0x10
-    FRAME_AT = 0x09
+    FRAME_TYPES = {
+        'AT':           0x08,
+        'AT_QPV':       0x09,
+        'TX':           0x10,
+        'EXPLICIT_TX':  0x11,
+        'REMOTE':       0x17,
+        'AT_RESP':      0x88,
+        'MODEM_STATUS': 0x8A,
+        'TX_STATUS':    0x8B,
+        'ROUTE_INFO':   0x8D,
+        'RX':           0x90,
+        'EXPLICIT_RX':  0x91,
+        'NODE_ID':      0x95,
+        'REMOTE_RESP':  0x97,
+    }
+
     BUF_FULL_TIMEOUT = 5
     BUF_GET_TIMEOUT = 10
 
-    SPECIAL_BYTES = (FRAME_DELIM, ESCAPE, XON, XOFF)
-    
     rx_queue = queue.Queue()
 
     def __init__(self, devfile, baud):
@@ -44,7 +58,7 @@ class XBee():
         frame_size = len(data) + 14     # tx api frame has 14 bytes overhead
         frame = bytearray(((frame_size >> 8) & 0x0FF, 
                            (frame_size & 0x0FF),
-                           self.FRAME_TX, 
+                           self.FRAME_TYPES['TX'],
                            0x01,
                            # destination
                            (dest >> 56) & 0x0FF,
@@ -86,19 +100,21 @@ class XBee():
         num_bytes = self.serial.in_waiting
         incoming = self.serial.read(num_bytes)
         sequence = incoming.split(bytes(b'\x7E'))        
-        print(sequence)
         for s in sequence:
             if (self.validate_frame(s)):
                 self.rx_queue.put(s, self.BUF_FULL_TIMEOUT)
                 received = True
 
         return received
+
+    def parse_frame(self):
+        pass
             
     def escape(self, data):
         escaped = bytearray()
         for byte in data:
-            if byte in self.SPECIAL_BYTES:
-                escaped.append(self.ESCAPE)
+            if byte in self.SPECIAL_BYTES.values():
+                escaped.append(self.SPECIAL_BYTES['ESCAPE'])
                 escaped.append(byte ^ 0x20)
             else:
                 escaped.append(byte)
@@ -109,7 +125,7 @@ class XBee():
         # create an iterator from range() so we can use next() on it
         q = iter(range(len(data)))
         for i in q:
-            if data[i] == self.ESCAPE:
+            if data[i] == self.SPECIAL_BYTES['ESCAPE']:
                 unescaped.append(data[i+1] ^ 0x20)
                 next(q, None) # skip the next iteration
             else:
@@ -121,7 +137,6 @@ class XBee():
         frame = self.unescape(frame)
         if (len(frame) < 5):
             # already know this isn't a valid frame: not enough overhead
-            print('invalid len')
             return False
         frame_len = (frame[0] << 8) | frame[1]
         # make sure frame length is accurate
