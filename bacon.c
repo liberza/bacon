f9d16631900d30f8dff27afdc1b70fede3b4e391
Nick Levesque <nick@levesque.io>
2016-11-14 21:30:07 -0500

    hadn't added the main file. awkward.

---
+++ bacon.c
@@ -0,0 +1,60 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <util/atomic.h>
+
+#include "xbee.h"
+#include "serial.h"
+#include "status.h"
+#include "solenoid.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define USART_BAUDRATE 1200
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+#define ever ;;
+
+volatile uint8_t rx_flag;
+
+int main(void)
+{
+    status_pin_init();
+    serial_init(BAUD_PRESCALE, DATA_BITS_8, STOP_BITS_1, PARITY_DISABLED);
+    xbee_init();
+    solenoid_init();
+    uint64_t peer;
+    uint16_t data_len;
+    uint8_t frame[MAX_BUF_SIZE];
+    uint16_t start, end;
+
+    sei();
+
+    for(ever)
+    {
+        rx(frame);
+        /* while(rx_flag == 0); */
+        /* rx_flag = 0; */
+        /* start = rbuf.start; */
+        /* end = rbuf.end; */
+        /* tx((uint8_t*)(rbuf.buf), MAX_BUF_SIZE, 0x000000000000FFFF, 0x00); */
+        /* tx((uint8_t*)(start), 2, 0x000000000000FFFF, 0x00); */
+        /* tx((uint8_t*)(end), 2, 0x000000000000FFFF, 0x00); */
+        if (frame[3] == FRAME_TYPES.RX)
+        {
+            peer = (uint64_t)frame[4] << 56 |
+                   (uint64_t)frame[5] << 48 |
+                   (uint64_t)frame[6] << 40 |
+                   (uint64_t)frame[7] << 32 |
+                   (uint64_t)frame[8] << 24 |
+                   (uint64_t)frame[9] << 16 |
+                   (uint64_t)frame[10] << 8 |
+                   (uint64_t)frame[11];
+
+            data_len = (((uint16_t)frame[1]<<8)|(uint16_t)frame[2]);
+            tx(frame+FRAME_OHEAD.RX-1, data_len-FRAME_OHEAD.RX+4, peer, 0x00);
+        }
+    }
+}
