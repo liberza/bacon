3cb3d3edf7ebe218fc8df9b88e33ed79a4b60a2c
conrad.ege@maine.edu <conrad.ege@maine.edu>
2016-12-12 18:49:24 -0500

    successfully reading pressure (bar) and temperature (celsius)

--- tests/test_alt.c
+++ tests/test_alt.c
@@ -24,7 +24,8 @@ int main(void)
 	uint16_t measure_delay = 1000;
 	char buffer[30];
 	buffer[29] = '\n';
-
+	
+	int32_t altitude;
 	uint32_t D1;
 	uint32_t D2;
 	uint16_t C[8];
@@ -35,9 +36,10 @@ int main(void)
 	int64_t SENS;
 	int i;
 	serial_init(BAUD_PRESCALE, DATA_BITS_8, STOP_BITS_1, PARITY_DISABLED);
-
 	sei();
 
+	for (i = 0; i < 8; i++){ C[i] = 0;}
+	wipe(buffer);
 	// SS, MOSI, and SCK as outputs
 	DDRB |= (1 << PB3) | (1 << PB5) | (1 << PB2);
 		
@@ -48,7 +50,7 @@ int main(void)
 	SPCR = (1 << SPE) | (1 << MSTR);
 									
 	// module should be reset upon power up.
-	cmd_reset(); 
+	//cmd_reset(); 
 												
 	for ( i = 0; i < 8; i++){
 		C[i] = cmd_prom(i);
@@ -56,23 +58,39 @@ int main(void)
 	
 	for(ever)
 	{
-		D1 = cmd_adc(CMD_ADC_D1 + CMD_ADC_256);
-		D2 = cmd_adc(CMD_ADC_D2 + CMD_ADC_4096);
-
+		D1 = cmd_adc(CMD_ADC_D1 + CMD_ADC_256); // read uncompensated pressure
+		D2 = cmd_adc(0x14);						// read uncompensated temperature
+		
+		// calculate 1st order pressure and temperature (datasheet)
 		dT = D2 - C[5]*pow(2,8);
 		OFF = C[2]*pow(2,17) + dT*C[4]/pow(2,6);
 		SENS = C[1]*pow(2,16) + dT*C[3]/pow(2,7);
 
-		T = (2000 + (dT*C[6])/pow(2,23))/100;
-		P = (((D1*SENS)/pow(2,21) - OFF)/pow(2,15))/100;
+		T = (2000 + (dT*C[6])/pow(2,23));
+		P = (((D1*SENS)/pow(2,21) - OFF)/pow(2,15));
 		
-		sprintf(buffer, "%ld", T);
+		/* 
+		 * Calculate altitude using simplified barometric equation:
+		 *
+		 * 					  /         1/5.255\
+		 * altitude = 44330 * \1 - (p/p0)      /
+		 *
+		 */
+
+		P /= P0;
+		P /= 100;
+		P = pow(P, (1/5.255));
+		P = 1 - P;
+		altitude = 44330 * P;
+		sprintf(buffer, "%ld", altitude);
 
 		for (i = 0; i < 30; i++){
 			put_byte(buffer[i]);
+			//put_byte('q');
 		}
 
 		_delay_ms(measure_delay);
+		wipe(buffer);
 	}
 }
 
